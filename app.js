55482d5b960d0de3decef700ebe35e362cf5c0be
Navya0298 <111457747+Navya0298@users.noreply.github.com>
2022-11-02 20:35:55 -0500

    modified

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var petRouter = require('./routes/pet');
+var gridbuildRouter= require('./routes/gridbuild');
+var selctorRouter= require('./routes/selector');
 
 var app = express();
 
@@ -21,6 +24,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/pet', petRouter);
+app.use('/gridbuild', gridbuildRouter);
+app.use('/selector', selctorRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/pet.js
+++ routes/pet.js
@@ -3,5 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('pet', { title: 'Search Results by pet breed' });
 });
+
+module.exports = router;
